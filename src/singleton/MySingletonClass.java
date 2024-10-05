da19e0c20ecca6847d61b0e892c325c6b1490a24
Naga Vivek <43722806+Naga-Vivek@users.noreply.github.com>
2024-10-05 13:33:44 +0530

    Added Singleton Pattern

---
+++ src/singleton/MySingletonClass.java
@@ -0,0 +1,27 @@
+package singleton;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MySingletonClass {
+    private static Lock lock = new ReentrantLock();
+    private static MySingletonClass instance;
+    private MySingletonClass() {
+
+    }
+    public static MySingletonClass getInstance() {
+        // Note : making the getInstance() synchronized, solves concurrency issue
+        // but performance wise not good as each thread will be waiting
+        // until the current thread completes executing the method.
+
+        // Double check locking - solves concurrency problem and also performance wise good
+        if(instance == null) {
+            lock.lock();
+            if (instance == null) {
+                instance = new MySingletonClass();
+            }
+            lock.unlock();
+        }
+        return instance;
+    }
+}
---
+++ src/singleton/SingletonClient.java
@@ -0,0 +1,39 @@
+package singleton;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SingletonClient {
+    public static void main(String[] args) throws InterruptedException {
+        Runnable runnable = () -> {
+            MySingletonClass mySingletonClassObj = MySingletonClass.getInstance();
+            System.out.println(Thread.currentThread().getName() + ": " + mySingletonClassObj);
+        };
+        // We can implement the run method of Runnable in two ways
+        // 1. Using a Anonymous class by .          new Runnable(){ // implementation to override run method };
+        // 2. Using Lambda Expression .             () -> {implementation}
+
+        // Unless double check locking is used ,
+        // Singleton Pattern by default doesn't make sense in multithreaded environment, as multiple threads can create multiple instances.
+
+/*        Thread t1 = new Thread(runnable);
+        t1.start();
+
+        Thread t2 = new Thread(runnable);
+        t2.start();
+
+        Thread t3 = new Thread(runnable);
+        t3.start();
+
+        Thread t4 = new Thread(runnable);
+        t4.start();*/
+
+        ExecutorService executorService = Executors.newFixedThreadPool(9);
+        executorService.execute(runnable);
+        for(int i=0;i<9;i++){
+            executorService.execute(runnable);
+        }
+        executorService.shutdown();
+
+    }
+}
